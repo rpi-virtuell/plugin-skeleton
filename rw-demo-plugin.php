8bdc1eefd71ee6bc4961c6b9672e1fd681c0b744
Joachim Happel <joachim.happel@gmail.com>
2022-02-16 20:06:52 +0100

    only ajax

--- rw-demo-plugin.php
+++ rw-demo-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name:       RW Demo Plugin
+ * Plugin Name: Ajax Demo Plugin
 */
 
 class AJAX_Demo_Plugin {
@@ -15,51 +15,80 @@ class AJAX_Demo_Plugin {
 
 	}
 
-	public function request_via_ajax($data) {
+    //Option page im Menü einhängen
+	public function add_options_page() {
 
-		wp_send_json($data);
+		add_options_page( 'Ajax Plugin Options Page',
+            'Ajax Plugin',
+            'manage_options',
+            'rw-ajax-plugin',
+            array($this, 'display_options_page')
+        );
+
+	}
+
+    //Hier kommt die Ajaxanfrage an  (@see line 11)
+	public function request_via_ajax() {
+
+        //$_POST auswerten
+		$search = isset($_POST['search_input'])?$_POST['search_input']:'';
+
+        //die Rückantwort muss immer in Form eines arrrays sein
+        $return = array(
+           'results' => array ("<hr>","<p>Es wurde nach $search gefragt</p>","<hr>")
+        );
+
+        //als json versenden
+        wp_send_json( $return );
 		die();
 	}
 
 
-	public function add_options_page() {
-
-		add_options_page( 'Ajax Plugin Options Page', 'Ajax Plugin', 'manage_options', 'rw-ajax-plugin', array($this, 'display_options_page') );
-
-	}
+    //inhalt der Optionspage in der auch das Ajax ausgeführt wird
 	public function display_options_page() {
 		?>
 		<h1>Ajax Plugin Options Page</h1>
-		<button class="test">Test</button>
+        <input id="suche" placeholder="Tippe was">
+        <div id="results">Ausgabe der Ergebnisse aus der Ajax-Abfrage</div>
 
 
 		<script>
 
-            $( document ).on( 'click', 'button', function() {
-                $.ajax({
-                    type: 'POST',
-                    url: ajaxurl,     // the variable ajaxurl is prepared by wp core
-                    data: {
-                        action: 'request_via_ajax',  // ajax action @see line 11
-                        search_input: 'test'
-                    },
-                    success: function (data, textStatus, XMLHttpRequest) {
-                        readData = $.parseJSON(data);
-                        console.log($.parseJSON(data));
+            // Script erst laden, wenn das Document vollständig ausgebout ist
+            jQuery( document ).ready(function ($){
 
-                        if($('#results')){
+                //Ajax soll ausgelöst werden wenn im Input Feld geschrieben wird
+                $( document ).on( 'keydown', '#suche', function() {
+                    //ajax anfrage via Javascript an server schicken
+                    $.ajax({
+                        type: 'POST',
+                        url: ajaxurl,                    // ajaxurl: global wp var
+                        data: {                          // daten die per POST an den Server geschickt werden sollen
+                            action: 'request_via_ajax',  // ajax action @see line 11
+                            search_input: $('#suche').val()
+                        },
 
-                            $('#results').append(  readData.toString()  );
+                        //Ajax anfrage hat geklappt
+                        success: function (data, textStatus, XMLHttpRequest) { //erfolgreiche anfrage
+
+                            if($('#results')){
+
+                                $('#results').html(''); //Ausgabe in das div#results schreiben:
+                                for(const result of data.results){
+
+                                    $('#results').append(  result  + '<br>');
+
+                                }
+                            }
+                        },
+
+                        //Ajax anfrage hat nicht geklappt
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            console.log(errorThrown);
                         }
-
-                    },
-                    error: function (XMLHttpRequest, textStatus, errorThrown) {
-                        console.log(errorThrown);
-                    }
+                    });
                 });
-
             });
-
 		</script>
 		<?php
 	}
